54b254b3a2d8cf9ba206e7edd1d57fd22aa7ea74
Blackman99 <1197160272@qq.com>
2022-12-20 15:18:46 +0800

    feat: divide style import build

---
+++ .changeset/metal-houses-share.md
@@ -0,0 +1,5 @@
+---
+"@casual-ui/react": patch
+---
+
+divide style build and components build
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     lib: {
-      entry: resolve(__dirname, './src/index.ts'),
+      entry: resolve(__dirname, './src/import-style.ts'),
       name: 'CasualUIReact',
       fileName: format => `casual-ui-react.${format}.js`,
     },
